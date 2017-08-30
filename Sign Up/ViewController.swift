b941bacc0ea6137ace8a6ccb35ce815eb637346b
Geng Xu <gengxu@GengdeMacBook-Pro.local>
2017-08-30 21:33:47 +1000

    Initial Commit

---
+++ Sign Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sign Up
+//
+//  Created by Geng Xu on 2017/8/30.
+//  Copyright © 2017年 Geng Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
